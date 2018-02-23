3035bd82496967ab8a4ab0b7561eb22d90849070
Francis <36746163+sawed28@users.noreply.github.com>
2018-02-23 09:11:01 -0800

    Add files via upload

---
+++ switch1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+            }
+        }
+    }
+}
+//Outputs "three"
