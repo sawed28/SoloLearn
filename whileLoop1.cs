11120d7447c0edbba666bb4b03f5367753d10881
Francis <36746163+sawed28@users.noreply.github.com>
2018-02-23 09:37:24 -0800

    Add files via upload

---
+++ whileLoop1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while(num < 6)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+    }
+}
