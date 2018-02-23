8d5b5e90264b335c7ad63a7035d3167969a83cb7
Francis <36746163+sawed28@users.noreply.github.com>
2018-02-23 09:27:34 -0800

    Add files via upload

---
+++ defaultCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 88;
+            switch (age)
+            {
+                case 16:
+                    Console.WriteLine("Too young");
+                    break;
+                case 42:
+                    Console.WriteLine("Adult");
+                    break;
+                case 70:
+                    Console.WriteLine("Senior");
+                    break;
+                default:
+                    Console.WriteLine("The default case");
+                    break;
+            }
+            //Outputs "The default case");
+        }
+    }
+}
