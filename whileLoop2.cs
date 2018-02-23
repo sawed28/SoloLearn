5be09b81a1de3405b4bf019f9f87543e8b4f8d26
Francis <36746163+sawed28@users.noreply.github.com>
2018-02-23 09:41:15 -0800

    Add files via upload

---
+++ whileLoop2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while(num < 6)
+            {
+                Console.WriteLine(num);
+                num += 2;
+            }
+        /* Outputs
+         1
+         2
+         3
+         5
+         */
+        }
+    }
+}
