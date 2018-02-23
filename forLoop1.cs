6d410be069c36c46b58d678edeb861dc9526208f
Francis <36746163+sawed28@users.noreply.github.com>
2018-02-23 09:57:12 -0800

    Add files via upload

---
+++ forLoop1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int x = 10; x < 15; x++)
+            {
+                Console.WriteLine("Value of x: {0}", x);
+            }
+            /* 
+             Value of x: 10
+             Value of x: 11
+             Value of x: 12
+             Value of x: 13
+             Value of x: 14
+            */
+        }
+    }
+}
